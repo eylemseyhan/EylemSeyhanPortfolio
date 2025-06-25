a6c061f34ca57938506588772edec3c635b24f7f
eylemseyhan <eylemseyhan@trakya.edu.tr>
2025-06-25 13:06:32 +0300

    updated main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
-import { sanitizeDirective } from './directives/sanitize'
 import { MotionPlugin } from '@vueuse/motion'
 import Tres from '@tresjs/core'
 
